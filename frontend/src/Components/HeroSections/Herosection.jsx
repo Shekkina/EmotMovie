54256dc35a76da243f509df40fa703468361a2c0
Jayabharathi2210 <22csec11@gmail.com>
2025-07-10 23:57:48 +0530

    jcommit-4.1
    Modified the hero section. Added scrolling features to the cards.

--- frontend/src/Components/HeroSections/Herosection.jsx
+++ frontend/src/Components/HeroSections/Herosection.jsx
@@ -1,23 +1,49 @@
+import React, { useRef } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import './Herosection.css';
 import movie1 from '../../assets/movie1.jpg';
 import movie2 from '../../assets/movie2.png';
 import movie3 from '../../assets/movie3.jpg';
+import movie4 from '../../assets/movie4.jpg';
+import movie5 from '../../assets/movie5.jpg';
+import movie6 from '../../assets/movie6.jpg';
+import movie7 from '../../assets/movie7.jpg';
 
-function Hero() {
+const Hero = () => {
+
+  const scrollRef = useRef(null);
+
+  const scrollLeft = () => {
+    scrollRef.current.scrollBy({ left: -300, behaviour: 'smooth'});
+  }
+
+  const scrollRight = () => {
+    scrollRef.current.scrollBy({ left: 300, behaviour: 'smooth'});
+  }
+
+  const movieCards = [movie1, movie2, movie3, movie4, movie5, movie6, movie7];
+
   return (
     <section className="hero">
       <div className="hero-text">
         <h1>Discover the Movie for your mood with <span>EmotMovies</span></h1>
         <p>Join in the emotional journey of movies and songs. We help you to pick a genre according to your mood.</p>
+
         <div className="hero-buttons">
           <button className="btn-start">Get Started</button>
         </div>
       </div>
 
-      <div className="hero-cards">
-        <img src={movie1} alt="card1" className="hero-card" />
-        <img src={movie2} alt="card2" className="hero-card" />
-        <img src={movie3} alt="card3" className="hero-card" />
+      <div className="scroll-wrapper">
+        <FaChevronLeft className="scroll-icon-left" onClick={scrollLeft} />
+
+        <div className="card-scroll" ref={scrollRef}>
+          {movieCards.map((img, index) => (
+            <img key={index} src={img} alt={`card${index}`} className="hero-card" />
+          ))}
+        </div>
+
+        <FaChevronRight className="scroll-icon-right" onClick={scrollRight} />
       </div>
     </section>
   );
