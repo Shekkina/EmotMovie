bbdd26e6621bf7f2620ec4e0d127495feb683a91
Jayabharathi2210 <22csec11@gmail.com>
2025-07-10 20:53:45 +0530

    jaya 2nd commit
    In this the navbar is fully completed.

---
+++ frontend/src/Components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import './Navbar.css';
+
+function NavigationBar() {
+  return (
+    <Navbar fixed="top" expand="lg" bg="dark" className="my-navbar">
+      <Container fluid>
+        <Navbar.Brand href="#home">EmotMovies</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse className="basic-navbar-nav">
+            {/* Navbar elements */}
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#movies">Movies</Nav.Link>
+            <Nav.Link href="#songs">Songs</Nav.Link>
+            <Nav.Link href="#aboutus">About us</Nav.Link>            
+          </Nav>
+
+          {/*Search bar */}
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success" className='search-button'>Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
